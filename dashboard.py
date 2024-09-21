a894527dc384ed1292a5397c30a9b0128d70f6cb
humra024 <72643236+humra024@users.noreply.github.com>
2024-09-21 15:15:21 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,6 +12,23 @@ import os
 import warnings
 warnings.filterwarnings('ignore')
 
+# Remove whitespace from the top of the page and sidebar
+st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 0rem;
+                    padding-bottom: 10rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 st.set_page_config(page_title="Superstore!", page_icon=":bar_chart", layout="wide")
 st.title(" :bar_chart: Superstpre EDA")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
